83fafad5b66d99fd9c7a0e9ff1b51dc33b068ab4
Naveen-git-h <naveendugana3@gmail.com>
2024-12-27 23:23:51 +0530

    admin Page complet

--- src/component/admin/AdminLaptops.jsx
+++ src/component/admin/AdminLaptops.jsx
@@ -1,97 +1,116 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import axios from "axios"
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./AdminLLaptops.css"; // Import external CSS file
+
 const AdminLaptops = () => {
-const [laptops,setLaptops]=useState([])
-const navigate=useNavigate()
-  const fetchLaptops=async()=>{
-    const res=await axios.get("http://localhost:9090/admin/laptops",{
-      headers:{
-          "Authorization":`Bearer ${localStorage.getItem(`token`)}`
-      }
-    })
+  const [laptops, setLaptops] = useState([]);
+  const navigate = useNavigate();
 
-    const {data}=res;
-    console.log(data)
-    setLaptops(data)
+  const fetchLaptops = async () => {
+    const res = await axios.get("http://localhost:9090/admin/laptops", {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem(`token`)}`,
+      },
+    });
 
-  }
+    const { data } = res;
+    console.log(data);
+    setLaptops(data);
+  };
 
-  useEffect(()=>{
-    fetchLaptops()
-  },[])
+  useEffect(() => {
+    fetchLaptops();
+  }, []);
 
   const delete_func = async (id) => {
     try {
-        const res = await axios.delete(`http://localhost:9090/admin/delete/laptops/${id}`,{
-          headers:{
-              "Authorization":`Bearer ${localStorage.getItem(`token`)}`
-          }
-        });
+      await axios.delete(`http://localhost:9090/admin/delete/laptops/${id}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem(`token`)}`,
+        },
+      });
+      setLaptops(laptops.filter((laptop) => laptop.id !== id)); // Update state after deletion
     } catch (error) {
-        console.error("Error deleting laptop:", error);
+      console.error("Error deleting laptop:", error);
     }
-};
+  };
 
+  const edit = (id, name, cost, quantity, description, discount) => {
+    navigate(
+      `/updatelaptops/${id}/${name}/${cost}/${quantity}/${description}/${discount}`
+    );
+  };
 
-const edit=(id,name,cost,quantity,description,discount)=>{
-     navigate(`/updatelaptops/${id}/${name}/${cost}/${quantity}/${description}/${discount}`)
-}
-
-const add = ()=>{
-    navigate("/insertlaptops")
-}
+  const add = () => {
+    navigate("/insertlaptops");
+  };
 
   return (
-    <>
-            
-            <table border={1}
-                   align="center"
-                   cellSpacing={5}
-                   cellPadding={5}>
-                <thead>
-                    <tr>
-                        <th>id</th>
-                        <th>image</th>
-                        <th>Naveen</th>
-                        <th>Quantity</th>
-                        <th>edit</th>
-                        <th>delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        laptops.map((element,index)=>{
-                            return(<tr key={index}>
-                                <td>{element.id}</td>
-                                <td>
-                                    <img src={element.image} width={50} alt="Error"></img>
-                                </td>
-                                <td>{element.name}</td>
-                                <td>{element.quantity}</td>
-                                
-                                <td>
-                                    <i className="fa fa-edit" 
-                                       onClick={()=>edit(element.id, element.name, element.cost,element.quantity,element.description,element.discount)}>
-                                    </i>
-                                </td>
-                                <td>
-                                    <i className="fa fa-trash" onClick={()=>delete_func(element.id)}></i>
-                                </td>
-                            </tr>)
-                        })
-                    }
-                </tbody>
-
-                <tfoot>
-                    <tr>
-                        <td colSpan={6}> <button onClick={add} style={{marginLeft:110}}>Add Employee</button></td>
-                    </tr>
-                </tfoot>
-            </table>
-        </>
-
-  )
-}
-
+    <div className="admin-laptops-container">
+      <table className="admin-laptops-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {laptops.map((element, index) => (
+            <tr key={index}>
+              <td>{element.id}</td>
+              <td>
+                <img
+                  src={element.image}
+                  alt="Laptop"
+                  className="laptop-image"
+                />
+              </td>
+              <td>{element.name}</td>
+              <td>{element.quantity}</td>
+              <td>
+                <button
+                  className="edit-button"
+                  onClick={() =>
+                    edit(
+                      element.id,
+                      element.name,
+                      element.cost,
+                      element.quantity,
+                      element.description,
+                      element.discount
+                    )
+                  }
+                >
+                  Edit
+                </button>
+              </td>
+              <td>
+                <button
+                  className="delete-button"
+                  onClick={() => delete_func(element.id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan={6} className="add-button-container">
+              <button className="add-button" onClick={add}>
+                Add Laptop
+              </button>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
+};
 export default AdminLaptops
--- src/component/admin/UpdateLaptops.jsx
+++ src/component/admin/UpdateLaptops.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import "./UpdateLaptops.css"
 
 const UpdateLaptopForm = () => {
 
